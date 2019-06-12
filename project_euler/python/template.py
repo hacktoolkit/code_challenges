35a56b45841d8a0055d545a763443f1e2ed28c7b
Jonathan Tsai <hello@jontsai.com>
2019-06-12 22:21:41 +0000

    update solution template with PEP8 spacing

--- project_euler/python/template.py
+++ project_euler/python/template.py
@@ -3,16 +3,20 @@ Solution by jontsai <hello@jontsai.com>
 """
 from utils import *
 
+
 EXPECTED_ANSWER = 0
+
 
 def solve():
     answer = None
     return answer
 
+
 def main():
     answer = solve()
 
     print 'Expected: %s, Answer: %s' % (EXPECTED_ANSWER, answer)
+
 
 if __name__ == '__main__':
     main()
