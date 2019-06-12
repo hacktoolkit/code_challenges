18433ebde83137b3f90d879fb300aad21a73d43f
Jonathan Tsai <hello@jontsai.com>
2019-06-12 21:57:52 +0000

    update 493 candidate solution structure

--- project_euler/python/493.py
+++ project_euler/python/493.py
@@ -8,14 +8,41 @@ Give your answer with nine digits after the decimal point (a.bcdefghij).
 
 Solution by jontsai <hello@jontsai.com>
 """
+import itertools
+
 from utils import *
 
 EXPECTED_ANSWER = 0
 
-# COLORS = [c for c in 'ROYGBIV']
+#COLORS = [c for c in 'ROYGBIV']
+#BALLS = list(itertools.chain.from_iterable([c * 10 for c in COLORS]))
 
+# Ball 1: 1.0 possibility to be distinct color
+# 
 
+def solve(num_picks):
+    answer = None
 
-answer = None
+    total_balls = 70
+    balls_picked = 1
+    expected_num_distinct_colors = 1
+    for k in xrange(balls_picked, num_picks):
+        remaining_balls = total_balls - balls_picked
+        remaining_balls_with_same_colors = (10 * expected_num_distinct_colors) - (balls_picked * expected_num_distinct_colors)
+        p_pick_ball_same_color = 1.0 * remaining_balls_with_same_colors / remaining_balls
+        p_pick_ball_diff_color = 1 - p_pick_ball_same_color
+        expected_num_distinct_colors += p_pick_ball_diff_color
 
-print 'Expected: %s, Answer: %s' % (EXPECTED_ANSWER, answer)
+    answer = expected_num_distinct_colors
+    return answer
+
+def main():
+    target = 20
+    for x in xrange(1, target + 1):
+        answer = solve(x)
+        print x, answer
+
+    print 'Expected: %s, Answer: %s' % (EXPECTED_ANSWER, answer)
+
+if __name__ == '__main__':
+    main()
