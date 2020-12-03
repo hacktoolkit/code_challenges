04897fe3eba9c9962707ccb873342aea2ccb1e69
Jonathan Tsai <hello@jontsai.com>
2020-12-03 17:23:37 +0000

    adventofcode 2020 problem 1

---
+++ adventofcode/2020/1.py
@@ -0,0 +1,35 @@
+def main():
+    answer = solve()
+    print(answer)
+
+
+def solve():
+    with open('1.in', 'r') as f:
+        numbers = [int(n.strip()) for n in f.readlines()]
+
+    TARGET_SUM = 2020
+
+    a = None
+    b = None
+
+    for i in range(len(numbers)):
+        for j in range(len(numbers)):
+            if i == j:
+                # same index, skip
+                pass
+            else:
+                x, y = numbers[i], numbers[j]
+                if x + y == TARGET_SUM:
+                    a, b = x, y
+                    break
+
+    if a is None or b is None:
+        raise Exception('No numbers found that sum to: %s' % TARGET_SUM)
+    else:
+        answer = a * b
+
+    return answer
+
+
+if __name__ == '__main__':
+    main()
