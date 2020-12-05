b69848f3cb69dc0ba3b14ce94bf3c4558cfdc458
Jonathan Tsai <hello@jontsai.com>
2020-12-05 13:31:37 -0800

    update template

--- adventofcode/2020/template.py
+++ adventofcode/2020/template.py
@@ -1,11 +1,21 @@
+INPUT_FILE = 'n.in'
+# INPUT_FILE = 'n.test.in'
+
 def main():
     answer = solve()
     print(answer)
 
 
 def solve():
+    data = ingest()
     answer = None
     return answer
+
+
+def ingest():
+    with open(INPUT_FILE, 'r') as f:
+        data = [line.strip() for line in f.readlines()]
+    return data
 
 
 if __name__ == '__main__':
