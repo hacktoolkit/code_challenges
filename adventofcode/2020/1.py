88ba6f5041b34ffff89c37824c6d1f8539901c2a
Jonathan Tsai <hello@jontsai.com>
2020-12-06 00:21:48 -0800

    refactor 1

--- adventofcode/2020/1.py
+++ adventofcode/2020/1.py
@@ -1,11 +1,19 @@
+from utils import ingest
+
+
+INPUT_FILE = '1.in'
+EXPECTED_ANSWERS = (1019904, 176647680, )
+
+
 def main():
-    answer = solve()
-    print(answer)
+    answers = (solve1(), solve2(), )
+    print(answers)
+    assert(answers == EXPECTED_ANSWERS)
 
 
-def solve():
-    with open('1.in', 'r') as f:
-        numbers = [int(n.strip()) for n in f.readlines()]
+def solve1():
+    data = ingest(INPUT_FILE)
+    numbers = [int(n) for n in data]
 
     TARGET_SUM = 2020
 
@@ -26,6 +34,34 @@ def solve():
         raise Exception('No numbers found that sum to: %s' % TARGET_SUM)
     else:
         answer = a * b
+
+    return answer
+
+
+def solve2():
+    data = ingest(INPUT_FILE)
+    numbers = [int(n) for n in data]
+
+    TARGET_SUM = 2020
+
+    a, b, c = None, None, None
+
+    for i in range(len(numbers)):
+        for j in range(len(numbers)):
+            for k in range(len(numbers)):
+                if i == j or i == k or j == k:
+                    # same index, skip
+                    pass
+                else:
+                    x, y, z = numbers[i], numbers[j], numbers[k]
+                    if x + y + z == TARGET_SUM:
+                        a, b, c = x, y, z
+                        break
+
+    if a is None or b is None or c is None:
+        raise Exception('No numbers found that sum to: %s' % TARGET_SUM)
+    else:
+        answer = a * b * c
 
     return answer
 
