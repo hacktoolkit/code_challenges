1f0d1c7966927dc0f5908529949fb029392191c6
Jonathan Tsai <hello@jontsai.com>
2020-12-07 05:33:53 -0800

    AoC 2015.12.02

---
+++ adventofcode/2015/2.py
@@ -0,0 +1,81 @@
+from utils import ingest
+
+
+INPUT_FILE = '2.in'
+EXPECTED_ANSWERS = (1588178, 3783758, )
+
+# INPUT_FILE = 'n.test.in'
+# EXPECTED_ANSWERS = (None, None, )
+
+
+def main():
+    solution = Solution()
+    answers = (solution.solve1(), solution.solve2(), )
+    print(answers)
+    assert(answers == EXPECTED_ANSWERS)
+
+
+class Solution:
+    def __init__(self):
+        self.data = ingest(INPUT_FILE)
+        self.presents = [Present(dimensions) for dimensions in self.data]
+
+    def solve1(self):
+        answer = sum([present.wrapping_paper_area for present in self.presents])
+        return answer
+
+    def solve2(self):
+        answer = sum([present.total_ribbon for present in self.presents])
+        return answer
+
+
+class Present:
+    def __init__(self, dimensions):
+        self.l, self.w, self.h = [int(x) for x in dimensions.split('x')]
+
+    @property
+    def surface_area(self):
+        l, w, h = self.l, self.w, self.h
+        area = 2*l*w + 2*w*h + 2*h*l
+        return area
+
+    @property
+    def lw_area(self):
+        return self.l * self.w
+
+    @property
+    def wh_area(self):
+        return self.w * self.h
+
+    @property
+    def lh_area(self):
+        return self.l * self.h
+
+    @property
+    def smallest_side_area(self):
+        return min([
+            self.lw_area,
+            self.wh_area,
+            self.lh_area,
+        ])
+
+    @property
+    def wrapping_paper_area(self):
+        return self.surface_area + self.smallest_side_area
+
+    @property
+    def smallest_perimeter(self):
+        a, b, c = sorted([self.l, self.w, self.h])
+        return 2*a + 2*b
+
+    @property
+    def volume(self):
+        return self.l * self.w * self.h
+
+    @property
+    def total_ribbon(self):
+        return self.smallest_perimeter + self.volume
+
+
+if __name__ == '__main__':
+    main()
