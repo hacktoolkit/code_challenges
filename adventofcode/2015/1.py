581f19fb954cf0b2fdf8cb2c6d5b636015e833c0
Jonathan Tsai <hello@jontsai.com>
2020-12-07 05:19:03 -0800

    AoC 2015.12.01

---
+++ adventofcode/2015/1.py
@@ -0,0 +1,42 @@
+from utils import ingest
+
+
+INPUT_FILE = '1.in'
+EXPECTED_ANSWERS = (74, 1795, )
+
+
+def main():
+    solution = Solution()
+    answers = (solution.solve1(), solution.solve2(), )
+    print(answers)
+    assert(answers == EXPECTED_ANSWERS)
+
+
+class Solution:
+    def __init__(self):
+        self.data = ingest(INPUT_FILE)
+        self.instructions = ''.join(self.data)
+
+    def solve1(self):
+        floor = 0
+        for c in self.instructions:
+            floor += 1 if c == '(' else -1 if c == ')' else 0
+
+        answer = floor
+        return answer
+
+    def solve2(self):
+        answer = None
+
+        floor = 0
+        for position, c in enumerate(self.instructions):
+            floor += 1 if c == '(' else -1 if c == ')' else 0
+            if floor == -1:
+                answer = position + 1
+                break
+
+        return answer
+
+
+if __name__ == '__main__':
+    main()
