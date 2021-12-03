a5be5cbad946ac3a82f2505124251c3de4cab0bd
Jonathan Tsai <hello@jontsai.com>
2021-12-02 21:39:38 -0800

    AoC 2021.12.03 solution

--- adventofcode/2021/03.py
+++ adventofcode/2021/03.py
@@ -1,3 +1,6 @@
+# Python Standard Library Imports
+import copy
+
 from utils import (
     BaseSolution,
     InputConfig,
@@ -8,10 +11,10 @@ from utils import (
 PROBLEM_NUM = '03'
 
 TEST_MODE = False
-TEST_MODE = True
+# TEST_MODE = True
 
-EXPECTED_ANSWERS = (None, None, )
-TEST_EXPECTED_ANSWERS = (None, None, )
+EXPECTED_ANSWERS = (4147524, 3570354, )
+TEST_EXPECTED_ANSWERS = (198, 230, )
 
 
 def main():
@@ -42,18 +45,120 @@ class Solution(BaseSolution):
         data = self.data
 
     def solve1(self):
-        #
-        # TODO: FILL THIS IN
-        #
-        answer = None
+        submarine = Submarine(self.data)
+
+        answer = submarine.power_consumption
         return answer
 
     def solve2(self):
-        #
-        # TODO: FILL THIS IN
-        #
-        answer = None
+        submarine = Submarine(self.data)
+
+        answer = submarine.life_support_rating
         return answer
+
+
+class Submarine:
+    def __init__(self, diagnostic_report):
+        self.diagnostic_report = diagnostic_report
+
+    def _count_bits(self, bin_numbers, index):
+        b1 = 0
+        b0 = 0
+
+        for n in bin_numbers:
+            b = n[index]
+            b1 += 1 if b == '1' else 0
+            b0 += 1 if b == '0' else 0
+
+        return b1, b0
+
+
+    def _most_frequent_bit(self, bin_numbers, index):
+        b1, b0 = self._count_bits(bin_numbers, index)
+        mfb = '1' if b1 >= b0 else '0'
+        return mfb
+
+    def _least_frequent_bit(self, bin_numbers, index):
+        b1, b0 = self._count_bits(bin_numbers, index)
+        lfb = '1' if b1 < b0 else '0'
+        return lfb
+
+    def bin2dec(self, b):
+      return int(b, base=2)
+
+    ##
+    # Part 1
+
+    @property
+    def gamma_rate(self):
+        # most common bit for each position
+        size = len(self.diagnostic_report[0])
+        new_bits = [
+            self._most_frequent_bit(self.diagnostic_report, i)
+            for i
+            in range(size)
+        ]
+        rate_bits = ''.join(new_bits)
+        rate = self.bin2dec(rate_bits)
+        return rate
+
+    @property
+    def epsilon_rate(self):
+        # least common bit for each position
+        size = len(self.diagnostic_report[0])
+        new_bits = [
+            self._least_frequent_bit(self.diagnostic_report, i)
+            for i
+            in range(size)
+        ]
+        rate_bits = ''.join(new_bits)
+        rate = self.bin2dec(rate_bits)
+        return rate
+
+    @property
+    def power_consumption(self):
+        return self.gamma_rate * self.epsilon_rate
+
+    ##
+    # Part 2
+
+    @property
+    def oxygen_generator_rating(self):
+        size = len(self.diagnostic_report[0])
+
+        bin_numbers = copy.copy(self.diagnostic_report)
+
+        for i in range(size):
+            mfb = self._most_frequent_bit(bin_numbers, i)
+            bin_numbers = [n for n in bin_numbers if n[i] == mfb]
+
+            if len(bin_numbers) == 1:
+                break
+
+        rating_bin = bin_numbers[0]
+        rating = self.bin2dec(rating_bin)
+        return rating
+
+    @property
+    def co2_scrubber_rating(self):
+        size = len(self.diagnostic_report[0])
+
+        bin_numbers = copy.copy(self.diagnostic_report)
+
+        for i in range(size):
+            lfb = self._least_frequent_bit(bin_numbers, i)
+            bin_numbers = [n for n in bin_numbers if n[i] == lfb]
+
+            if len(bin_numbers) == 1:
+                break
+
+        rating_bin = bin_numbers[0]
+        rating = self.bin2dec(rating_bin)
+        return rating
+
+    @property
+    def life_support_rating(self):
+        return self.oxygen_generator_rating * self.co2_scrubber_rating
 
 
 if __name__ == '__main__':
