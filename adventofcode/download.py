0c69a31dbf88c582ff79e49652ebd4d6b89a7d2e
Jonathan Tsai <hello@jontsai.com>
2021-12-06 23:02:58 -0800

    adds a script to download the problem input

---
+++ adventofcode/download.py
@@ -0,0 +1,37 @@
+# Python Standard Library Imports
+import argparse
+import os
+
+# Third Party (PyPI) Imports
+import requests
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+
+SESSION = os.environ.get('SESSION')
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('day', type=int)
+    args = parser.parse_args()
+
+    download_input(args.day)
+
+
+def download_input(day):
+    url = f'https://adventofcode.com/2021/day/{day}/input'
+
+    cookies = {
+        'session': SESSION,
+    }
+    response = requests.get(url, cookies=cookies)
+    filename = f'{str(day).zfill(2)}.in'
+    with open(filename, 'w') as f:
+        f.write(response.content.decode())
+
+
+if __name__ == '__main__':
+    main()
