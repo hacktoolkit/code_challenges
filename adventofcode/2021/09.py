ce2639b5fa7f9a625d1e52f21826626003348ad6
Jonathan Tsai <hello@jontsai.com>
2021-12-08 22:49:07 -0800

    AoC 2021.12.09

--- adventofcode/2021/09.py
+++ adventofcode/2021/09.py
@@ -10,10 +10,10 @@ from utils import (
 PROBLEM_NUM = '09'
 
 TEST_MODE = False
-TEST_MODE = True
+# TEST_MODE = True
 
-EXPECTED_ANSWERS = (None, None, )
-TEST_EXPECTED_ANSWERS = (None, None, )
+EXPECTED_ANSWERS = (558, 882942, )
+TEST_EXPECTED_ANSWERS = (15, 1134, )
 
 
 def main():
@@ -23,8 +23,9 @@ def main():
         as_json=False,
         as_groups=False,
         as_oneline=False,
-        as_table=False,
-        cell_func=None
+        as_table=True,
+        row_func=lambda x: x[0],
+        cell_func=lambda x: [int(d) for d in str(x)]
     )
 
     if TEST_MODE:
@@ -43,20 +44,125 @@ def main():
 class Solution(BaseSolution):
     def process_data(self):
         data = self.data
+        self.height_map = HeightMap(data)
 
     def solve1(self):
-        #
-        # TODO: FILL THIS IN
-        #
-        answer = None
+        answer = self.height_map.risk_score
         return answer
 
     def solve2(self):
-        #
-        # TODO: FILL THIS IN
-        #
-        answer = None
+        basins = self.height_map.basins
+        basin_sizes =[len(basin) for basin in basins]
+        a, b, c = sorted(basin_sizes, reverse=True)[:3]
+        answer = a * b * c
         return answer
+
+
+class HeightMap:
+    def __init__(self, table):
+        self.table = table
+        self.num_rows = len(table)
+        self.num_cols = len(table[0])
+
+    def __str__(self):
+        buf = []
+        for x in range(self.num_rows):
+            buf.append(','.join([str(_) for _ in self.table[x]]))
+            buf.append('\n')
+
+        s = ''.join(buf)
+        return s
+
+    def is_valid_coord(self, x, y):
+        is_valid = (
+            0 <= x < self.num_rows
+            and 0 <= y < self.num_cols
+        )
+        return is_valid
+
+    def neighbor_coords(self, x, y):
+        neighbor_shifts = [
+            (0, 1,),
+            (0, -1,),
+            (-1, 0,),
+            (1, 0,),
+        ]
+
+        coords = []
+        for (dx, dy) in neighbor_shifts:
+            a, b = (x + dx, y + dy)
+            if self.is_valid_coord(a, b):
+                coords.append((a, b))
+
+        return coords
+
+    @property
+    def low_points(self):
+        points = []
+
+        for x in range(self.num_rows):
+            for y in range(self.num_cols):
+                value = self.table[x][y]
+                is_low_point = True  # innocent until proven guilty
+
+                neighbor_coords = self.neighbor_coords(x, y)
+                for (a, b) in neighbor_coords:
+                    neighbor = self.table[a][b]
+                    if neighbor <= value:
+                        is_low_point = False
+                        break
+
+                if is_low_point:
+                    points.append((x, y))
+
+        return points
+
+    def risk_level(self, x, y):
+        return 1 + self.table[x][y]
+
+    @property
+    def risk_score(self):
+        score = sum([
+            self.risk_level(x, y)
+            for (x, y)
+            in self.low_points
+        ])
+        return score
+
+    @property
+    def basins(self):
+        basins = [
+            self.find_basin(x, y)
+            for x, y
+            in self.low_points
+        ]
+        return basins
+
+    def find_basin(self, x, y):
+        visited = {
+            (x, y): True,
+        }
+
+        to_visit = self.neighbor_coords(x, y)
+        while len(to_visit) > 0:
+            a, b = to_visit.pop()
+            value = self.table[a][b]
+            if value < 9:
+                visited[(a,b)] = True
+                neighbor_coords = self.neighbor_coords(a, b)
+                for coord in neighbor_coords:
+                    if coord not in visited:
+                        to_visit.append(coord)
+            else:
+                visited[(a,b)] = False
+
+        basin_coords = [
+            coord
+            for coord, in_basin
+            in visited.items()
+            if in_basin
+        ]
+        return basin_coords
 
 
 if __name__ == '__main__':
