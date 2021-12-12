d9702af32874ec013a0aa5e8adc386b8390a5c6d
Jonathan Tsai <hello@jontsai.com>
2021-12-12 18:33:08 +0000

    clean up AoC 2021.12.01

--- adventofcode/2021/01.py
+++ adventofcode/2021/01.py
@@ -6,6 +6,11 @@ from utils import (
 )
 
 
+PROBLEM_NUM = '01'
+
+TEST_MODE = False
+# TEST_MODE = True
+
 EXPECTED_ANSWERS = (1521, 1543, )
 TEST_EXPECTED_ANSWERS = (7, 5, )
 
@@ -13,15 +18,23 @@ TEST_EXPECTED_ANSWERS = (7, 5, )
 def main():
     input_config = InputConfig(
         as_integers=True,
+        as_comma_separated_integers=False,
         as_json=False,
         as_groups=False,
         as_oneline=False,
         as_table=False,
+        row_func=None,
         cell_func=None
     )
 
-    #solution = Solution('01.in', input_config, EXPECTED_ANSWERS)
-    solution = Solution('01.test.in', input_config, TEST_EXPECTED_ANSWERS)
+    if TEST_MODE:
+        input_filename = f'{PROBLEM_NUM}.test.in'
+        expected_answers = TEST_EXPECTED_ANSWERS
+    else:
+        input_filename = f'{PROBLEM_NUM}.in'
+        expected_answers = EXPECTED_ANSWERS
+
+    solution = Solution(input_filename, input_config, expected_answers)
 
     solution.solve()
     solution.report()
@@ -33,27 +46,23 @@ class Solution(BaseSolution):
         pass
 
     def solve1(self):
-        answer = None
-
         num_increases = 0
         prev_value = None
 
         for value in self.data:
             if prev_value is None:
                 pass
+            elif value > prev_value:
+                num_increases += 1
             else:
-                if value > prev_value:
-                    num_increases += 1
+                pass
+
             prev_value = value
 
         answer = num_increases
-
-        self.answer1 = answer
         return answer
 
     def solve2(self):
-        answer = None
-
         window_size = 3
         num_increases = 0
         prev_subtotal = None
@@ -61,8 +70,7 @@ class Solution(BaseSolution):
         for i in range(len(self.data) - window_size + 1):
             values = self.data[i:i + window_size]
             subtotal = sum(values)
-            print(values, subtotal)
-
+            # print(values, subtotal)
 
             if prev_subtotal is None:
                 pass
@@ -74,8 +82,6 @@ class Solution(BaseSolution):
             prev_subtotal = subtotal
 
         answer = num_increases
-
-        self.answer2 = answer
         return answer
 
 
