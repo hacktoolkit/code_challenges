570d3705e2fe8e5e6f0b99127b0e130c53bdd674
Jonathan Tsai <hello@jontsai.com>
2021-12-12 22:26:54 -0800

    AoC 2021.12.13

--- adventofcode/2021/13.py
+++ adventofcode/2021/13.py
@@ -1,6 +1,3 @@
-# Python Standard Library Imports
-from collections import defaultdict
-
 from utils import (
     BaseSolution,
     InputConfig,
@@ -10,10 +7,10 @@ from utils import (
 PROBLEM_NUM = '13'
 
 TEST_MODE = False
-TEST_MODE = True
+# TEST_MODE = True
 
-EXPECTED_ANSWERS = (None, None, )
-TEST_EXPECTED_ANSWERS = (None, None, )
+EXPECTED_ANSWERS = (638, 97, )
+TEST_EXPECTED_ANSWERS = (17, 16, )
 
 
 def main():
@@ -21,7 +18,7 @@ def main():
         as_integers=False,
         as_comma_separated_integers=False,
         as_json=False,
-        as_groups=False,
+        as_groups=True,
         as_oneline=False,
         as_table=False,
         row_func=None,
@@ -44,20 +41,96 @@ def main():
 class Solution(BaseSolution):
     def process_data(self):
         data = self.data
+        raw_coords, raw_folds = data
+        self.paper = Paper(raw_coords, raw_folds)
 
     def solve1(self):
-        #
-        # TODO: FILL THIS IN
-        #
-        answer = None
+        paper = self.paper
+        paper.perform_fold()
+
+        answer = paper.num_visible
         return answer
 
     def solve2(self):
-        #
-        # TODO: FILL THIS IN
-        #
-        answer = None
+        paper = self.paper
+        paper.perform_all_folds()
+
+        print(paper)
+
+        with open(f'{PROBLEM_NUM}.out', 'w') as f:
+            f.write(str(paper))
+
+        answer = paper.num_visible
         return answer
+
+
+class Paper:
+    def __init__(self, raw_coords, raw_folds):
+        self.coords = [
+            [int(c) for c in raw_coord.split(',')]
+            for raw_coord
+            in raw_coords
+        ]
+
+        self.grid = set()
+        for (x, y) in self.coords:
+            self.grid.add((x, y))
+
+        self.reset_bounds()
+
+        folds = [
+            raw_fold.removeprefix('fold along ').split('=')
+            for raw_fold
+            in raw_folds
+        ]
+        self.folds = [
+            [fold[0], int(fold[1])]
+            for fold
+            in folds
+        ]
+
+        self.fold_n = 0
+
+    def __str__(self):
+        buf = []
+        for y in range(self.max_y + 1):
+            for x in range(self.max_x + 1):
+                is_visible = (x,y) in self.grid
+                c = '#' if is_visible else '.'
+                buf.append(c)
+            buf.append('\n')
+
+        s = ''.join(buf)
+        return s
+
+    def reset_bounds(self):
+        self.max_x = max([coord[0] for coord in self.grid])
+        self.max_y = max([coord[1] for coord in self.grid])
+
+    def perform_fold(self):
+        fold = self.folds[self.fold_n]
+        self.fold_n += 1
+        fold_dimension, fold_position = fold
+
+        for x in range(self.max_x + 1):
+            for y in range(self.max_y + 1):
+                dim = x if fold_dimension == 'x' else y
+                if (x,y) in self.grid and dim > fold_position:
+                    d = dim - fold_position
+                    new_dim = fold_position - d
+                    self.grid.remove((x,y))
+                    new_coord = (new_dim, y) if fold_dimension == 'x' else (x, new_dim)
+                    self.grid.add(new_coord)
+
+        self.reset_bounds()
+
+    def perform_all_folds(self):
+        while self.fold_n < len(self.folds):
+            self.perform_fold()
+
+    @property
+    def num_visible(self):
+        return len(self.grid)
 
 
 if __name__ == '__main__':
