3b8145b5e02010b491dcf20fa251ca38e727d2a6
Jonathan Tsai <hello@jontsai.com>
2021-12-13 21:30:11 -0800

    AoC 2021.12.14 part 1

--- adventofcode/2021/14.py
+++ adventofcode/2021/14.py
@@ -1,4 +1,5 @@
 # Python Standard Library Imports
+import itertools
 from collections import defaultdict
 
 from utils import (
@@ -12,8 +13,16 @@ PROBLEM_NUM = '14'
 TEST_MODE = False
 TEST_MODE = True
 
-EXPECTED_ANSWERS = (None, None, )
-TEST_EXPECTED_ANSWERS = (None, None, )
+EXPECTED_ANSWERS = (3213, None, )
+TEST_EXPECTED_ANSWERS = (1588, 2188189693529, )
+
+
+
+def pairwise(iterable):
+    # pairwise('ABCDEFG') --> AB BC CD DE EF FG
+    a, b = itertools.tee(iterable)
+    next(b, None)
+    return zip(a, b)
 
 
 def main():
@@ -21,7 +30,7 @@ def main():
         as_integers=False,
         as_comma_separated_integers=False,
         as_json=False,
-        as_groups=False,
+        as_groups=True,
         as_oneline=False,
         as_table=False,
         row_func=None,
@@ -44,20 +53,66 @@ def main():
 class Solution(BaseSolution):
     def process_data(self):
         data = self.data
+        raw_polymer, raw_rules = data
+
+        self.polymer = raw_polymer[0]
+        self.rules = dict([
+            raw_rule.split(' -> ')
+            for raw_rule
+            in raw_rules
+        ])
+
+        self.step_n = 0
 
     def solve1(self):
-        #
-        # TODO: FILL THIS IN
-        #
-        answer = None
+        for _ in range(10):
+            self.step()
+
+        counts = defaultdict(int)
+        for c in self.polymer:
+            counts[c] += 1
+
+        answer = max(list(counts.values())) - min(list(counts.values()))
         return answer
 
     def solve2(self):
-        #
-        # TODO: FILL THIS IN
-        #
-        answer = None
+        for _ in range(30):
+            self.step()
+
+        counts = defaultdict(int)
+        for c in self.polymer:
+            counts[c] += 1
+
+        answer = max(list(counts.values())) - min(list(counts.values()))
         return answer
+
+    def step(self):
+        polymer = self.polymer
+        rules = self.rules
+
+        def mod_pair(pair):
+            if pair in rules:
+                result = pair[0] + rules[pair] + pair[1]
+            else:
+                result = pair
+            return result
+
+        new_pairs = [mod_pair(''.join(pair)) for pair in pairwise(polymer)]
+        new_polymer = self.merge_pairs(new_pairs)
+
+        self.polymer = new_polymer
+        self.step_n += 1
+
+    def merge_pairs(self, pairs):
+        l = len(pairs)
+        buf = []
+        for i, pair in enumerate(pairs):
+            if i == 0:
+                buf.append(pair)
+            else:
+                buf.append(pair[1:])
+        new_polymer = ''.join(buf)
+        return new_polymer
 
 
 if __name__ == '__main__':
