60491fd57ca88f758fa0aa15d4e667dcf2188cd1
Jonathan Tsai <hello@jontsai.com>
2021-12-21 21:44:05 -0800

    update AoC 2021.12.22

--- adventofcode/2021/22.py
+++ adventofcode/2021/22.py
@@ -13,13 +13,17 @@ PROBLEM_NUM = '22'
 TEST_MODE = False
 # TEST_MODE = True
 
-EXPECTED_ANSWERS = (None, None, )
+EXPECTED_ANSWERS = (610196, None, )
 
 # TEST_VARIANT = 'a'
 # TEST_EXPECTED_ANSWERS = (39, None, )
 
-TEST_VARIANT = 'b'
-TEST_EXPECTED_ANSWERS = (590784, None, )
+# TEST_VARIANT = 'b'
+# TEST_EXPECTED_ANSWERS = (590784, None, )
+
+TEST_VARIANT = 'c'
+TEST_EXPECTED_ANSWERS = (474140, 2758514936282235, )
+
 
 def main():
     input_config = InputConfig(
@@ -57,8 +61,8 @@ class Solution(BaseSolution):
         instructions = self.instructions
 
         for instruction in instructions:
-            for coord in instruction.get_coords():
-                if Instruction.in_region(*coord):
+            for coord in instruction.get_init_coords():
+                if Instruction.in_initialization_region(*coord):
                     if instruction.on:
                         cubes_on.add(coord)
                     elif coord in cubes_on:
@@ -70,24 +74,32 @@ class Solution(BaseSolution):
                     # do nothing
                     pass
 
-        count = 0
-        for x, y, z in cubes_on:
-            if Instruction.in_region(x, y, z):
-                count += 1
-
-        answer = count
+        answer = len(cubes_on)
         return answer
 
     def solve2(self):
-        #
-        # TODO: FILL THIS IN
-        #
-        answer = None
+        cubes_on = set()
+
+        instructions = self.instructions
+
+        for instruction in instructions:
+            for coord in instruction.get_coords():
+                if instruction.on:
+                    cubes_on.add(coord)
+                elif coord in cubes_on:
+                    cubes_on.remove(coord)
+                else:
+                    # do nothing
+                    pass
+
+        answer = len(cubes_on)
         return answer
 
 
 class Instruction:
     REGEX = re.compile(r'(?P<on_or_off>on|off) x=(?P<x1>-?\d+)\.\.(?P<x2>-?\d+),y=(?P<y1>-?\d+)\.\.(?P<y2>-?\d+),z=(?P<z1>-?\d+)\.\.(?P<z2>-?\d+)')
+    INIT_RANGE_LOWER = -50
+    INIT_RANGE_UPPER = 50
 
     def __init__(self, raw_instruction):
         m = Instruction.REGEX.match(raw_instruction)
@@ -114,30 +126,48 @@ class Instruction:
             raise Exception('Illegal format')
 
     @classmethod
-    def in_region(cls, x, y, z):
-        in_region = (
-            -50 <= x <= 50
-            and -50 <= y <= 50
-            and -50 <= z <= 50
+    def in_initialization_region(cls, x, y, z):
+        in_initialization_region = (
+            Instruction.INIT_RANGE_LOWER <= x <= Instruction.INIT_RANGE_UPPER
+            and Instruction.INIT_RANGE_LOWER <= y <= Instruction.INIT_RANGE_UPPER
+            and Instruction.INIT_RANGE_LOWER <= z <= Instruction.INIT_RANGE_UPPER
         )
-        return in_region
+        return in_initialization_region
 
-    def get_xrange(self):
-        x1 = max(-50, self.x1)
-        x2 = min(50, self.x2)
+    def get_init_xrange(self):
+        x1 = max(Instruction.INIT_RANGE_LOWER, self.x1)
+        x2 = min(Instruction.INIT_RANGE_UPPER, self.x2)
         for x in range(x1, x2 + 1):
             yield x
 
-    def get_yrange(self):
-        y1 = max(-50, self.y1)
-        y2 = min(50, self.y2)
+    def get_init_yrange(self):
+        y1 = max(Instruction.INIT_RANGE_LOWER, self.y1)
+        y2 = min(Instruction.INIT_RANGE_UPPER, self.y2)
         for y in range(y1, y2 + 1):
             yield y
 
-    def get_zrange(self):
-        z1 = max(-50, self.z1)
-        z2 = min(50, self.z2)
+    def get_init_zrange(self):
+        z1 = max(Instruction.INIT_RANGE_LOWER, self.z1)
+        z2 = min(Instruction.INIT_RANGE_UPPER, self.z2)
         for z in range(z1, z2 + 1):
+            yield z
+
+    def get_init_coords(self):
+        for x in self.get_init_xrange():
+            for y in self.get_init_yrange():
+                for z in self.get_init_zrange():
+                    yield (x, y, z)
+
+    def get_xrange(self):
+        for x in range(self.x1, self.x2 + 1):
+            yield x
+
+    def get_yrange(self):
+        for y in range(self.y1, self.y2 + 1):
+            yield y
+
+    def get_zrange(self):
+        for z in range(self.z1, self.z2 + 1):
             yield z
 
     def get_coords(self):
