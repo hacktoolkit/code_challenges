cff2be29ba138c6c80706cb6d7585906e0468216
Jonathan Tsai <hello@jontsai.com>
2021-12-21 20:54:01 -0800

    stubbed AoC 2021.12.22

---
+++ adventofcode/2021/20.py
@@ -0,0 +1,64 @@
+# Python Standard Library Imports
+from collections import defaultdict
+
+from utils import (
+    BaseSolution,
+    InputConfig,
+)
+
+
+PROBLEM_NUM = '20'
+
+TEST_MODE = False
+TEST_MODE = True
+
+EXPECTED_ANSWERS = (None, None, )
+TEST_EXPECTED_ANSWERS = (None, None, )
+
+
+def main():
+    input_config = InputConfig(
+        as_integers=False,
+        as_comma_separated_integers=False,
+        as_json=False,
+        as_groups=False,
+        as_oneline=False,
+        as_table=False,
+        row_func=None,
+        cell_func=None
+    )
+
+    if TEST_MODE:
+        input_filename = f'{PROBLEM_NUM}.test.in'
+        expected_answers = TEST_EXPECTED_ANSWERS
+    else:
+        input_filename = f'{PROBLEM_NUM}.in'
+        expected_answers = EXPECTED_ANSWERS
+
+    solution = Solution(input_filename, input_config, expected_answers)
+
+    solution.solve()
+    solution.report()
+
+
+class Solution(BaseSolution):
+    def process_data(self):
+        data = self.data
+
+    def solve1(self):
+        #
+        # TODO: FILL THIS IN
+        #
+        answer = None
+        return answer
+
+    def solve2(self):
+        #
+        # TODO: FILL THIS IN
+        #
+        answer = None
+        return answer
+
+
+if __name__ == '__main__':
+    main()
