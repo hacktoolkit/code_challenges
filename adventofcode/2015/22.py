29635c9563db391bcae722976049f95a893d4ab8
Jonathan Tsai <hello@jontsai.com>
2022-12-04 18:09:33 -0800

    update AoC 2015.12.22 solution formatting

--- adventofcode/2015/22.py
+++ adventofcode/2015/22.py
@@ -1,6 +1,7 @@
 # Python Standard Library Imports
 import copy
 import math
+import pathlib
 import re
 import typing as T
 from collections import defaultdict
@@ -14,37 +15,49 @@ from itertools import (
     permutations,
 )
 
+# Third Party (PyPI) Imports
+import click
+
 from utils import (
     BaseSolution,
     InputConfig,
 )
 
 
-PROBLEM_NUM = '22'
+YEAR = int(pathlib.Path.cwd().parts[-1])
+DAY = int(pathlib.Path(__file__).stem)
+PROBLEM_NUM = str(DAY).zfill(2)
 
-TEST_MODE = False
-# TEST_MODE = True
+TEST_MODE = True
 
 EXPECTED_ANSWERS = (None, None)
-TEST_VARIANT = 'b'  # '', 'b', 'c', 'd', ...
+TEST_VARIANT = ''  # '', 'b', 'c', 'd', ...
 TEST_EXPECTED_ANSWERS = {
-    '': (226, None),
-    'b': (641, None),
+    '': (None, None),
+    'b': (None, None),
     'c': (None, None),
 }
 
 DEBUGGING = False
-# DEBUGGING = True
 
 
-def debug(s):
+def debug(*args):
     if DEBUGGING:
-        print(s)
+        print(*args)
     else:
         pass
 
 
-def main():
+@click.command()
+@click.option('--is_real', '--real', is_flag=True, default=False)
+@click.option('--submit', is_flag=True, default=False)
+@click.option('--is_debug', '--debug', is_flag=True, default=False)
+def main(is_real, submit, is_debug):
+    global TEST_MODE
+    global DEBUGGING
+    TEST_MODE = not is_real
+    DEBUGGING = is_debug
+
     input_config = InputConfig(
         as_integers=False,
         as_comma_separated_integers=False,
@@ -65,9 +78,17 @@ def main():
         input_filename = f'{PROBLEM_NUM}.in'
         expected_answers = EXPECTED_ANSWERS
 
-    solution = Solution(input_filename, input_config, expected_answers)
+    solution = Solution(
+        input_filename,
+        input_config,
+        expected_answers,
+        year=YEAR,
+        day=DAY,
+    )
 
     solution.solve()
+    if submit:
+        solution.submit(is_test=TEST_MODE)
     solution.report()
 
 
