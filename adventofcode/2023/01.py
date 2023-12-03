72436bc75d55b28efe55cb366078ec293b6a62a6
Jonathan Tsai <hello@jontsai.com>
2023-12-03 17:01:39 +0000

    AoC 2023.12.01 - part 1

--- adventofcode/2023/01.py
+++ adventofcode/2023/01.py
@@ -37,44 +37,93 @@ from utils import (
 )
 
 
-config.EXPECTED_ANSWERS = (None, None)
+config.EXPECTED_ANSWERS = (56506, None)
 config.TEST_CASES = {
-    '': (None, None),
-    # 'b': (None, None),
+    # '': (142, 142),
+    'b': (209, 281),
     # 'c': (None, None),
 }
-
-config.INPUT_CONFIG.as_integers = False
-config.INPUT_CONFIG.as_comma_separated_integers = False
-config.INPUT_CONFIG.as_json = False
-config.INPUT_CONFIG.as_groups = False
-config.INPUT_CONFIG.strip_lines = True
-config.INPUT_CONFIG.as_oneline = False
-config.INPUT_CONFIG.as_coordinates = False
-config.INPUT_CONFIG.coordinate_delimeter = None
-config.INPUT_CONFIG.as_table = False
-config.INPUT_CONFIG.row_func = None
-config.INPUT_CONFIG.cell_func = None
 
 
 @solution
 class Solution(BaseSolution):
     def process_data(self):
-        data = self.data
+        self.doc = CalibrationDoc(self.data)
 
     def solve1(self):
-        #
-        # TODO: FILL THIS IN
-        #
-        answer = None
+        answer = sum(self.doc.calibration_values)
         return answer
 
     def solve2(self):
-        #
-        # TODO: FILL THIS IN
-        #
-        answer = None
+        answer = sum(self.doc.calibration_values_with_word_digits)
         return answer
+
+
+class CalibrationDoc:
+    NUMBERS = {
+        'one': 1,
+        'two': 2,
+        'three': 3,
+        'four': 4,
+        'five': 5,
+        'six': 6,
+        'seven': 7,
+        'eight': 8,
+        'nine': 9,
+    }
+
+    def __init__(self, data):
+        self.data = data
+
+    @property
+    def calibration_values(self):
+        return self.extract_calibration_values()
+
+    @property
+    def calibration_values_with_word_digits(self):
+        return self.extract_calibration_values(word_digits=True)
+
+    def extract_calibration_values(self, word_digits=False):
+        values = [
+            self._extract_value(line, word_digits=word_digits)
+            for line in self.data
+        ]
+        return values
+
+    def _extract_value(self, raw_line, word_digits=False):
+        line = (
+            self._convert_words_to_digits(raw_line) if word_digits else raw_line
+        )
+        digits = [c for c in line if c.isdigit()] or [0]
+        value = int(str(digits[0]) + str(digits[-1]))
+        return value
+
+    def _convert_words_to_digits(self, raw_line):
+        line = raw_line
+
+        did_replace = True
+
+        while did_replace:
+            # replacements happen left to right
+            indexes = sorted(
+                [
+                    (index, word)
+                    for word, digit in self.NUMBERS.items()
+                    if (index := line.find(word)) >= 0
+                ],
+                key=lambda x: x[0],
+            )
+
+            if len(indexes) > 0:
+                _, word = indexes[0]
+                line = line.replace(word, str(self.NUMBERS[word]))
+                did_replace = True
+            else:
+                did_replace = False
+
+        print(raw_line, line)
+
+        return line
 
 
 if __name__ == '__main__':
